d99526183ff7fe23e55668a301afd2a02fe7a75f
Casper <casper.boon@uwa.edu.au>
2022-03-16 13:49:58 +0800

    added deprection notice for nml database file; updated copyright notices

--- src/aed_test.F90
+++ src/aed_test.F90
@@ -9,14 +9,14 @@
 !#                                                                             #
 !#      http://aquatic.science.uwa.edu.au/                                     #
 !#                                                                             #
-!#  Copyright 2015 - 2021 -  The University of Western Australia               #
+!#  Copyright 2015 - 2022 -  The University of Western Australia               #
 !#                                                                             #
-!#   GLM is free software: you can redistribute it and/or modify               #
+!#   AED is free software: you can redistribute it and/or modify               #
 !#   it under the terms of the GNU General Public License as published by      #
 !#   the Free Software Foundation, either version 3 of the License, or         #
 !#   (at your option) any later version.                                       #
 !#                                                                             #
-!#   GLM is distributed in the hope that it will be useful,                    #
+!#   AED is distributed in the hope that it will be useful,                    #
 !#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
 !#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
 !#   GNU General Public License for more details.                              #
@@ -37,7 +37,7 @@ MODULE aed_test
 !-------------------------------------------------------------------------------
 ! aed_test --- test model
 !
-! The AED2 module test contains basic equations that have no dependencies
+! The AED module test contains basic equations that have no dependencies
 !-------------------------------------------------------------------------------
    USE aed_core
 
